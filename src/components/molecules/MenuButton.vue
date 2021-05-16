dceb54e0b4f552461b07005623aea4cf7725ff5f
boleyelemeneye <daniil@capusta.space>
2021-05-16 16:57:59 +0300

    mechanics of displaying the catalog menu

--- src/components/molecules/MenuButton.vue
+++ src/components/molecules/MenuButton.vue
@@ -1,8 +1,50 @@
 <template>
   <div :class="$style.container">
-    burger-icon
+    <button @click="onToggleActiveState">
+      {{ isActiveState ? 'burger-icon-active' : 'burger-icon-inactive' }}
+    </button>
   </div>
 </template>
+
+<script>
+import { EVENT_BUS } from 'root/eventBus';
+import { CATALOG_MENU_OPENED, CATALOG_MENU_CLOSED } from 'root/const';
+
+export default {
+  name: 'MenuButton',
+  created() {
+    EVENT_BUS.$on(CATALOG_MENU_CLOSED, this.onResetActiveState);
+  },
+  beforeDestroy() {
+    EVENT_BUS.$off(CATALOG_MENU_CLOSED, this.onResetActiveState);
+  },
+  data() {
+    return {
+      isActiveState: false,
+    };
+  },
+  watch: {
+    isActiveState: {
+      handler(val) {
+        if (val) {
+          EVENT_BUS.$emit(CATALOG_MENU_OPENED);
+          return;
+        }
+
+        EVENT_BUS.$emit(CATALOG_MENU_CLOSED);
+      },
+    },
+  },
+  methods: {
+    onResetActiveState() {
+      this.isActiveState = false;
+    },
+    onToggleActiveState() {
+      this.isActiveState = !this.isActiveState;
+    },
+  },
+};
+</script>
 
 <style lang="sass" module>
   .container
--- src/components/organisms/CatalogMenu.vue
+++ src/components/organisms/CatalogMenu.vue
@@ -1,8 +1,44 @@
 <template>
   <div :class="$style.container">
-    catalog-menu
+    <div>{{ isShownCatalogMenu ? 'catalog-menu-shown' : 'catalog-menu-close' }}</div>
+    <div>
+      <button @click="onClickToCloseCatalogMenu">Close</button>
+    </div>
   </div>
 </template>
+
+<script>
+import { EVENT_BUS } from 'root/eventBus';
+import { CATALOG_MENU_OPENED, CATALOG_MENU_CLOSED } from 'root/const';
+
+export default {
+  name: 'CatalogMenu',
+  created() {
+    EVENT_BUS.$on(CATALOG_MENU_OPENED, this.onShowCatalogMenu);
+    EVENT_BUS.$on(CATALOG_MENU_CLOSED, this.onCloseCatalogMenu);
+  },
+  beforeDestroy() {
+    EVENT_BUS.$off(CATALOG_MENU_OPENED, this.onShowCatalogMenu);
+    EVENT_BUS.$off(CATALOG_MENU_CLOSED, this.onCloseCatalogMenu);
+  },
+  data() {
+    return {
+      isShownCatalogMenu: false,
+    };
+  },
+  methods: {
+    onShowCatalogMenu() {
+      this.isShownCatalogMenu = true;
+    },
+    onCloseCatalogMenu() {
+      this.isShownCatalogMenu = false;
+    },
+    onClickToCloseCatalogMenu() {
+      EVENT_BUS.$emit(CATALOG_MENU_CLOSED);
+    },
+  },
+};
+</script>
 
 <style lang="sass" module>
   .container
