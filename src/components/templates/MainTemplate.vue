f37051ffebdf4b5485b0b5aa67b4009cf727c50c
boleyelemeneye <daniil@capusta.space>
2021-05-22 21:38:00 +0300

    link button type transparent

--- src/components/templates/MainTemplate.vue
+++ src/components/templates/MainTemplate.vue
@@ -2,11 +2,29 @@
   <div>
     <main-header />
     <catalog-menu />
+
     <div style="margin: 10rem; width: 315px; display: flex;">
-      <link-button link="//ya.ru" text="Прайс-лист" />
+      <link-button link="//ya.ru" text="Прайс-лист">
+        <download-icon slot="icon" />
+      </link-button>
     </div>
+
     <div style="margin: 10rem; width: 315px; display: flex;">
-      <link-button type="secondary" link="//ya.ru" text="Каталог продукции" />
+      <link-button type="secondary" link="//ya.ru" text="Каталог продукции">
+        <download-icon slot="icon" />
+      </link-button>
+    </div>
+
+    <div style="margin: 10rem; width: 315px; display: flex;">
+      <link-button type="secondary" link="//ya.ru" text="Все категории">
+        <chevron-right-icon slot="icon" />
+      </link-button>
+    </div>
+
+    <div style="margin: 10rem; width: 315px; display: flex;">
+      <link-button type="transparent" link="//ya.ru" text="Подробнее">
+        <chevron-right-icon slot="icon" />
+      </link-button>
     </div>
 
     <pulsar-software-information />
@@ -14,6 +32,9 @@
 </template>
 
 <script>
+import DownloadIcon from 'icons/base/download.svg';
+import ChevronRightIcon from 'icons/arrows/chevron-right.svg';
+
 import LinkButton from 'molecules/LinkButton';
 
 import MainHeader from 'organisms/MainHeader';
@@ -23,6 +44,9 @@ import PulsarSoftwareInformation from 'organisms/PulsarSoftwareInformation';
 export default {
   name: 'MainTemplate',
   components: {
+    DownloadIcon,
+    ChevronRightIcon,
+
     LinkButton,
     MainHeader,
     CatalogMenu,
